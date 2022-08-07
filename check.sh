bd893e720666f153b80ace20c560c96de5b053d2
User_R <95095147+UserR256@users.noreply.github.com>
2022-08-07 11:13:44 +0200

    typo

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ PRCY='prcycoind'
 
 clear
 echo "Checking if prcycoind is running..."
-if ps ax | grep -v grep | grep $PRCY> /dev/null
+if ps aux | grep -v grep | grep $PRCY> /dev/null
 then
     echo "$PRCY is running, no further action required"
 else
